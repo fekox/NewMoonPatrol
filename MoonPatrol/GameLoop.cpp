50323159038965be4c04473e7358c060ac6e634a
nico <nicorm2000@gmail.com>
2022-11-01 10:18:28 -0300

    Added Collision function RECT RECT

--- MoonPatrol/GameLoop.cpp
+++ MoonPatrol/GameLoop.cpp
@@ -5,6 +5,8 @@ static void Initialize();
 
 static void Close();
 
+bool CollisionRectangleRectangle(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h);
+
 void RunGame()
 {
 	Initialize();
@@ -35,4 +37,13 @@ static void Initialize()
 static void Close()
 {
 	CloseWindow();
+}
+
+bool CollisionRectangleRectangle(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h)
+{
+	if (r1x + r1w >= r2x && r1x <= r2x + r2w && r1y + r1h >= r2y && r1y <= r2y + r2h) 
+	{    
+		return true;
+	}
+	return false;
 }
