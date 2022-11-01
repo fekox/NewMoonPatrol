9f94efc1966a1a06f6b1a826d636028ed71ba199
nico <nicorm2000@gmail.com>
2022-11-01 10:10:52 -0300

    Added version number

--- MoonPatrol/GameLoop.cpp
+++ MoonPatrol/GameLoop.cpp
@@ -1,6 +1,38 @@
-#include "GameLoop.h"s
+#include "GameLoop.h"
+#include "raylib.h"
+
+static void Initialize();
+
+static void Close();
 
 void RunGame()
 {
+	Initialize();
+	HideCursor();
+	SetExitKey(KEY_NULL);
 
+	bool playingGame = true;
+
+	while (playingGame && !WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		DrawText("VERSION 0.1", 10, 10, 20, GREEN);
+
+		EndDrawing();
+	}
+
+	Close();
+}
+
+static void Initialize()
+{
+	InitWindow(1024, 768, "MoonPatrol");
+}
+
+static void Close()
+{
+	CloseWindow();
 }
