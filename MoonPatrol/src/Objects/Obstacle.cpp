75a1da6d9f943fcc9339c14ad9dd8fafe94a68c3
Facundo Santos <facusantos123@gmail.com>
2022-11-16 23:38:47 -0300

    Add the obstacle

---
+++ MoonPatrol/src/Objects/Obstacle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Objects/Obstacle.h"
+
+Obstacle CreateObstacle(int screenWidth, int screenHeight)
+{
+	Obstacle obstacle;
+
+	obstacle.pos.x = static_cast<float>(screenWidth - 30);
+	obstacle.pos.y = static_cast<float>(screenHeight / 1.2);
+
+	obstacle.width = 30;
+	obstacle.height = 60;
+
+	obstacle.speed = 400;
+
+	obstacle.color = BLUE;
+
+	return obstacle;
+}
+
+void DrawObstacle(Obstacle& obstacle)
+{
+	DrawRectangle(static_cast<int>(obstacle.pos.x), static_cast<int>(obstacle.pos.y), static_cast<int>(obstacle.width), static_cast<int>(obstacle.height), obstacle.color);
+}
---
+++ MoonPatrol/src/Objects/Obstacle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "raylib.h"
+
+struct Obstacle
+{
+	Vector2 pos;
+
+	float width;
+	float height;
+
+	float speed;
+
+	Color color;
+};
+
+Obstacle CreateObstacle(int screenWidth, int screenHeight);
+
+void DrawObstacle(Obstacle& obstacle);
