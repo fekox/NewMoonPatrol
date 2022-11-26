4b016488835f6bb339cec4079c25b93cb14d2c41
Facundo Santos <facusantos123@gmail.com>
2022-11-26 14:43:53 -0300

    Fix the .bat file

--- bat/post_build_commands.bat
+++
@@ -1,3 +0,0 @@
-IF exist "$(SolutionDir)MoonPatrol\bin\$(Platform)\$(Configuration)\resources" rd "$(SolutionDir)MoonPatrol\bin\$(Platform)\$(Configuration)\resources"
-MKDIR  "$(SolutionDir)MoonPatrol\bin\$(Platform)\$(Configuration)\resources"
-XCOPY "$(SolutionDir)MoonPatrol\resources" "$(SolutionDir)MoonPatrol\bin\$(Platform)\$(Configuration)\resources"/e /h /y
